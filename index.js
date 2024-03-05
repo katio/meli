4605ba39a1ebf5d053d698cf6cb303e079697d95
Johann Echavarria <johannechavarria@hotmail.com>
2024-03-05 16:27:42 -0500

    nodemon, start scripts, search limit & search parser

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const app = express();
 const axios = require('axios')
 
 const PORT = process.env.PORT || 5000;
-const SEARCH_API_URL = 'https://api.mercadolibre.com/sites/MLA/search?q=:query';
+const SEARCH_API_URL = 'https://api.mercadolibre.com/sites/MLA/search?q=:query&limit=4';
 const ITEM_DETAILS_API_URL = 'https://api.mercadolibre.com/items/:id';
 const ITEM_DESCRIPTION_API_URL = 'https://api.mercadolibre.com/items/:id/description';
 
-const error500Response = (res, description, err) =>{
-  res.status(500).json({description, err});
+function error500Response(err) {
+  const {res, msg = ''} = this;
+  console.log({error500Response: msg, err})
+  res.status(500).json({msg, err});
 }
 
 //const simpleErrorResponse = (err) => console.log({err});
@@ -19,14 +21,39 @@ const successResponse = (res, json) =>  res.status(200).json(json);
 
 const searchItemsParser = (res, searchResponse) => {
   const {data = {}} = searchResponse;
-  successResponse(res, data);
+  const categories = data.available_filters
+  ?.find(filter => id='category')
+  ?.values?.map(category => category.name) || [];
+  const parsedData = {
+    author: {
+      name: 'Johann',
+      lastname: 'Echavarría',
+    },
+    categories,
+    items: data?.results.map(item => ({
+      id: item.id ?? "N/A",
+      title: item.title ?? "N/A",
+      price: {
+        currency: item.currency_id ?? "N/A",
+        amount: Math.floor(item.price ?? 0),
+        decimals: +((item.price ?? 0) % 1).toFixed(2).substring(2)
+      },
+      picture: item.thumbnail ?? "N/A",
+      condition: item.condition ?? "N/A",
+      free_shipping: item.shipping?.free_shipping ?? false
+    }))
+  }
+
+  successResponse(res, parsedData);
+  //successResponse(res, data);
 }
 
 const fetchSearchResults = (res, queryParam) => {
   const apiURL = SEARCH_API_URL.replace(':query', queryParam);
   axios.get(apiURL)
   .then(searchItemsParser.bind(null, res))
-  .catch(error500Response.bind(null, res, 'Error fetching item search'));
+  .catch(error500Response.bind({res, msg: 'Error fetching item search'}));
+  //.catch(simpleErrorResponse);
 }
 
 
