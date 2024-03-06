47f8b236fc4747b387a86dc4481e6b69ce07ad65
Johann Echavarria <johannechavarria@hotmail.com>
2024-03-06 02:41:31 -0500

    SearchBar.tsx working

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './SearchBar.css';
 
 const SearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearchSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const trimmedQuery = searchQuery.trim();
+    if (trimmedQuery) {
+      navigate(`/items?search=${trimmedQuery}`);
+    }
+  };
 
   return (
-    <div className="searchbar">
-      <input className='searchbar__inputbox'></input>
-      <button>Search</button>
-    </div>
+    <form className="searchbar" onSubmit={handleSearchSubmit}>
+      <input
+        className='searchbar__inputbox'
+        value={searchQuery}
+        onChange={handleSearchChange}
+      ></input>
+      <button type="submit">Search</button>
+    </form>
   );
 };
 
-
 export default SearchBar;
+
