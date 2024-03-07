863b144b87297cb437dae57f20341caeaa560434
Johann Echavarria <johannechavarria@hotmail.com>
2024-03-06 21:34:17 -0500

    Results page styles

--- client/src/pages/SearchResultsPage/SearchResultsPage.tsx
+++ client/src/pages/SearchResultsPage/SearchResultsPage.tsx
@@ -2,29 +2,40 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { useQuery } from "react-query";
 import fetchSearchResults from '../../services/fetchSearchResults';
-import './SearchResultsPage.css';
+import styles from './SearchResultsPage.module.scss';
 
-const useQuerySearch = () =>  new URLSearchParams(useLocation().search);
 
+const useQuerySearch = () => new URLSearchParams(useLocation().search);
 
 const SearchResultsPage = () => {
+
   const query = useQuerySearch();
   const searchQuery = query.get('search') || '';
+
   const { isLoading, error, data } = useQuery(['searchResults', searchQuery], () => fetchSearchResults(searchQuery), {
     enabled: !!searchQuery,
   });
 
-  if (isLoading) return <div>Cargando resultados...</div>;
-  if (error) return <div>Error al cargar los resultados</div>;
+  if (isLoading) return <div>Loading results...</div>;
+
+  if (error) return <div>Error loading the results</div>;
 
   return (
-    <div className="searchresultspage">
+    <div className={styles.searchResultsPage}>
       {data?.items.map((item: any) => (
-        <div key={item.id}>{item.title}</div>
+        <div key={item.id} className={styles.itemCard}>
+          <div className={styles.itemImageContainer}>
+            <img src={item.picture} alt={item.title} className={styles.itemImage} />
+          </div>
+          <div className={styles.itemDetails}>
+            <span className={styles.itemPrice}>${item.price.amount.toLocaleString()}</span>
+            <span className={styles.itemTitle}>{item.title}</span>
+          </div>
+          <div className={styles.itemLocation}>{item.location || 'Capital Federal'}</div>
+        </div>
       ))}
     </div>
   );
 };
-
 
 export default SearchResultsPage;
